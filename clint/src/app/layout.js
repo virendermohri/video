4085a7d0953ff0319f8c83ee88b1f1154281a0c5
virendermohri <virendermohri@gamil.com>
2025-05-31 16:18:04 +0530

    ads

--- clint/src/app/layout.js
+++ clint/src/app/layout.js
@@ -1,13 +1,12 @@
 import Footer from '@/components/Footer';
 import './globals.css';
 import Navbar from '@/components/Navbar';
-import AdBanner from '@/components/AdBanner';
 
-// export const metadata = {
-//   title: 'DesiBhabi – Adult Content Platform',
-//   description: 'Watch hot desi videos online. Fully SEO-optimized adult content platform.',
-//   name: "6a97888e-site-verification", content: "9ec7ad82c743680208838033783755bb"
-// };
+export const metadata = {
+  title: 'DesiBhabi – Adult Content Platform',
+  description: 'Watch hot desi videos online. Fully SEO-optimized adult content platform.',
+  name: "6a97888e-site-verification", content: "9ec7ad82c743680208838033783755bb"
+};
 
 
 export default function RootLayout({ children }) {
@@ -15,13 +14,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
       <head>
-        
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="6a97888e-site-verification" content="9ec7ad82c743680208838033783755bb"></meta>
       </head>
       <body className="">
+       <iframe src="//a.magsrv.com/iframe.php?idzone=5626584&size=300x250" width="300" height="250" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
         <Navbar />
-        <AdBanner/>
         {children}
         
         <Footer />
--- clint/src/components/AdBanner.js
+++ clint/src/components/AdBanner.js
@@ -1,23 +1,20 @@
 // components/AdIframe.js
 import React from "react";
 
-const AdBanner = ({ src }) => {
+const AdBanner = ({ src, width = "300", height = "250" }) => {
   return (
-    <div className=" max-w-4xl mx-auto m-2 p-4">
+    <div className="my-4">
       <iframe
-        src={"//a.magsrv.com/iframe.php?idzone=5626584&size=728x90"}
-        width="100%"
-        height="90"
-       className=""
+        src={src}
+        width={width}
+        height={height}
         scrolling="no"
         marginWidth="0"
         marginHeight="0"
         frameBorder="0"
         allow="autoplay"
         title="Ad"
-      >nj</iframe>
-        
-{/* <iframe src="//a.magsrv.com/iframe.php?idzone=5626584&size=728x90" width="728" height="90" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe> */}
+      ></iframe>
     </div>
   );
 };
